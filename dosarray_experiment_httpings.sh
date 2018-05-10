98b72e6b876eae7699306a8988708d97be3aa3be
Shilpi Bose <shilpi296@gmail.com>
2018-05-10 12:15:55 -0400

    Setting CURRENT_HOST_IP from config values instead of hardcoding it

--- dosarray_experiment_httpings.sh
+++ dosarray_experiment_httpings.sh
@@ -15,14 +15,10 @@ then
 fi
 source "${DOSARRAY_SCRIPT_DIR}/dosarray_config.sh"
 
-# NOTE CURRENT_HOST_IP==2 since we skip the first host, where we run the target server.
-# FIXME horrible hack -- instead of hardcoding the initial IP use DOSARRAY_VIRT_NET_SUFFIX
-CURRENT_HOST_IP=${DOSARRAY_VIRT_NET_SUFFIX[0]}
-
-# NOTE the first host since we're running the server there.
+# NOTE skipping the first host since we're running the server there.
 for IDX in `dosarray_physical_hosts_skip 1`
 do
-  CURRENT_HOST_IP=$((CURRENT_HOST_IP+1))
+  CURRENT_HOST_IP=${DOSARRAY_VIRT_NET_SUFFIX[${IDX}]}
   HOST_NAME="${DOSARRAY_PHYSICAL_HOSTS_PUB[${IDX}]}"
   HOST_IP="${DOSARRAY_VIRT_NET_PREFIX}0.${CURRENT_HOST_IP}"
   echo "Starting httpings in $HOST_NAME (${HOST_IP})"
