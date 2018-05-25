00b8757c5e4a7b676ff3b607803147f00d635783
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-05-25 09:54:12 -0400

    added more examples of using dosarray;

---
+++ src/dosarray_experiment_more_examples.sh
@@ -0,0 +1,44 @@
+#/bin/sh -e
+# Example of using DoSarray
+# Nik Sultana, February 2018, UPenn
+#
+# Targetting Apache using various attacks
+# NOTE we can also easily change this to target Nginx and others.
+
+dosarray_execute_on "${HOST_NAME}" "${EXPERIMENT_RESET_CMD}"
+sleep ${INTER_EXPERIMENT_GAP}
+
+export EXPERIMENT_TAG=ge
+echo "Running ${EXPERIMENT_TAG} at $(date)"
+echo "  Writing to ${RESULT_DIR_PREFIX}${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}" # FIXME repeated below
+DESTINATION_DIR=${RESULT_DIR_PREFIX}${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX} \
+TITLE="Apache worker, GoldenEye, ${EXPERIMENT_SET}" \
+./dosarray_run_experiment.sh apache_worker goldeneye \
+> ${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}_output.stdout \
+2> ${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}_output.stderr
+
+dosarray_execute_on "${HOST_NAME}" "${EXPERIMENT_RESET_CMD}"
+sleep ${INTER_EXPERIMENT_GAP}
+
+export EXPERIMENT_TAG=th
+echo "Running ${EXPERIMENT_TAG} at $(date)"
+echo "  Writing to ${RESULT_DIR_PREFIX}${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}" # FIXME repeated below
+DESTINATION_DIR=${RESULT_DIR_PREFIX}${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX} \
+TITLE="Apache worker, Tors Hammer, ${EXPERIMENT_SET}" \
+./dosarray_run_experiment.sh apache_worker torshammer \
+> ${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}_output.stdout \
+2> ${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}_output.stderr
+
+dosarray_execute_on "${HOST_NAME}" "${EXPERIMENT_RESET_CMD}"
+sleep ${INTER_EXPERIMENT_GAP}
+
+export EXPERIMENT_TAG=baseline
+echo "Running ${EXPERIMENT_TAG} at $(date)"
+echo "  Writing to ${RESULT_DIR_PREFIX}${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}" # FIXME repeated below
+DESTINATION_DIR=${RESULT_DIR_PREFIX}${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX} \
+TITLE="Apache worker, baseline, ${EXPERIMENT_SET}" \
+./dosarray_run_experiment.sh apache_worker none \
+> ${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}_output.stdout \
+2> ${EXPERIMENT_TAG}${RESULT_DIR_SUFFIX}_output.stderr
+
+dosarray_execute_on "${HOST_NAME}" "${EXPERIMENT_RESET_CMD}"
