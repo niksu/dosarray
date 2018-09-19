3810f9c50db52a0e7028fe56c16aab2df8dc4ed5
Shilpi Bose <shilpi296@gmail.com>
2018-09-19 00:38:08 -0400

    Add title to command line and other changes in plotting

--- src/dosarray_graphing_focus.sh
+++ src/dosarray_graphing_focus.sh
@@ -6,11 +6,11 @@
 # Use of this source code is governed by the Apache 2.0 license; see LICENSE
 #
 # $ export DOSARRAY_VERSION="0.3"
-# $ DOSARRAY_NHIST_RESULT=1 python generate_availability_chart.py '../example_experiment/c*log' > availability_focus.data
-# $ ../dosarray_graphing_focus.sh -i availability_focus.data -i focus_graph.pdf
+# $ DOSARRAY_HIST_FOCUS=10 python generate_availability_chart.py '../example_experiment/c*log' > availability_focus.data
+# $ ./dosarray_graphing_focus.sh -i availability_focus.data -o focus_graph.pdf -t availability
 #
 
-while getopts "i:o" opt; do
+while getopts "i:o:t:" opt; do
   case ${opt} in
     i )
       INPUT_FILE=$OPTARG
@@ -18,8 +18,11 @@ while getopts "i:o" opt; do
     o )
       OUTPUT_FILE=$OPTARG
       ;;
+    t )
+      TITLE=$OPTARG
+      ;;
     ? )
-      echo "Usage: ./dosarray_graphing_focus -i <input-file> -o <output-file>"
+      echo "Usage: ./dosarray_graphing_focus -i <input-file> -o <output-file> -t <title>"
       exit 1
       ;;
   esac
@@ -35,9 +38,8 @@ set ylabel 'instances (%%)' rotate parallel # number of instances, whose latency
 SUFFIX="\n\
 set yrange [0:100] \n\
 set style data lines \n\
-set title '${TITLE}' \n\
 set output '${OUTPUT_FILE}' \n\
-plot '${INPUT_FILE}' using 1:2 smooth csplines title 'availability' \n\
+plot '${INPUT_FILE}' using 2:3 smooth csplines title '${TITLE}'\n\
 "
 
 MIDDLE=""
