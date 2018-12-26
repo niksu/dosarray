a1bf26b193f942fb0d79a57cc8439e33dd7974f6
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-12-26 11:43:58 -0500

    removed redirection to DESTINATION_DIR since we're already in that directory;

--- src/dosarray_run_experiment_graphing.sh
+++ src/dosarray_run_experiment_graphing.sh
@@ -24,10 +24,10 @@ then
 fi
 
 cd ${DESTINATION_DIR}
-command time ${DOSARRAY_SCRIPT_DIR}/src/generate_availability_chart.py "${DOSARRAY_LOG_NAME_PREFIX}*.log" > ${DESTINATION_DIR}/availability.data
+command time ${DOSARRAY_SCRIPT_DIR}/src/generate_availability_chart.py "${DOSARRAY_LOG_NAME_PREFIX}*.log" > availability.data
 
 #generate availability data for 'availability over time' plot
-DOSARRAY_NHIST_RESULT=1 python ${DOSARRAY_SCRIPT_DIR}/src/generate_availability_chart.py "${DOSARRAY_LOG_NAME_PREFIX}*.log" > ${DESTINATION_DIR}/availability_filtered.data
+DOSARRAY_NHIST_RESULT=1 python ${DOSARRAY_SCRIPT_DIR}/src/generate_availability_chart.py "${DOSARRAY_LOG_NAME_PREFIX}*.log" > availability_filtered.data
 
 # And finally we graph it to produce the summary plot, contour plot and availability plot respectively
 ${DOSARRAY_SCRIPT_DIR}/src/dosarray_graphing.sh -i "${DESTINATION_DIR}/availability.data" -o "${DESTINATION_DIR}/graph.pdf" "${TITLE}" "${DOSARRAY_ATTACK_STARTS_AT}" "$((DOSARRAY_ATTACK_STARTS_AT+DOSARRAY_ATTACK_LASTS_FOR))"
