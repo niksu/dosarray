9b9e811e06da53eb6773c350166338d0238d2932
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2019-03-07 10:39:57 -0500

    improving output: both the interface and more compact indication of success vs failure;

--- src/dosarray_check_hosts.sh
+++ src/dosarray_check_hosts.sh
@@ -3,6 +3,9 @@
 # Nik Sultana, March 2019, UPenn
 #
 # Use of this source code is governed by the Apache 2.0 license; see LICENSE
+#
+# FIXME we only support IPv4
+# FIXME various hard-codes, such as "docker_bridge"
 
 if [ -z "${DOSARRAY_SCRIPT_DIR}" ]
 then
@@ -15,6 +18,8 @@ then
 fi
 source "${DOSARRAY_SCRIPT_DIR}/config/dosarray_config.sh"
 
+DOCKER_BRIDGE=docker_bridge
+
 tput sgr0
 tput rmso
 tput rmul
@@ -24,23 +29,148 @@ for IDX in ${DOSARRAY_CONTAINER_HOST_IDXS}
 do
   HOST_NAME="${DOSARRAY_PHYSICAL_HOSTS_PUB[${IDX}]}"
   HOST_IP="${DOSARRAY_PHYSICAL_HOSTS_PRIV[${IDX}]}"
+  tput setaf 4
+  #tput smul
   echo -n "Checking "
   tput smso
   echo -n "$HOST_NAME"
   tput rmso
-  echo " (${HOST_IP})\n"
+  echo -n " (${HOST_IP})"
+  tput sgr0
+  #tput rmul
+  echo ""
 
-  dosarray_execute_on "${HOST_NAME}" "" "docker --version"
-  dosarray_execute_on "${HOST_NAME}" "" "docker images | grep dosarray"
-  dosarray_execute_on "${HOST_NAME}" "" "ifconfig ${DOSARRAY_HOST_INTERFACE_MAP[${IDX}]}"
-  dosarray_execute_on "${HOST_NAME}" "" "ifconfig docker_bridge"
-  VIRTUAL_NETWORK="${DOSARRAY_VIRT_NETS[${IDX}]}0"
-  dosarray_execute_on "${HOST_NAME}" "" "route | grep ${VIRTUAL_NETWORK}"
+#  echo -n "  Checking host reachable: "
+#  dosarray_execute_on "${HOST_NAME}" "uname -a" "-q" 2>&1 > /dev/null
+#  RESULT="$?"
+#  if [ "$RESULT" == "0" ]
+#  then
+#    tput smso
+#    echo "OK"
+#    tput rmso
+#  else
+#    tput smso
+#    echo "FAIL ($RESULT)"
+#    tput rmso
+#    break
+#  fi
+#
+#  echo -n "  Checking has Docker: "
+#  dosarray_execute_on "${HOST_NAME}" "docker --version" "-q" 2>&1 > /dev/null
+#  RESULT="$?"
+#  if [ "$RESULT" == "0" ]
+#  then
+#    tput smso
+#    echo "OK"
+#    tput rmso
+#  else
+#    tput smso
+#    echo "FAIL ($RESULT)"
+#    tput rmso
+#    break
+#  fi
+#
+#  echo -n "  Checking has DoSarray image: "
+#  dosarray_execute_on "${HOST_NAME}" "docker images | grep dosarray" "-q" 2>&1 > /dev/null
+#  RESULT="$?"
+#  if [ "$RESULT" == "0" ]
+#  then
+#    tput smso
+#    echo "OK"
+#    tput rmso
+#  else
+#    tput smso
+#    echo "FAIL ($RESULT)"
+#    tput rmso
+#    break
+#  fi
 
-  dosarray_execute_on "${HOST_NAME}" "" "sudo sh -c 'iptables -S | grep -E \"^-A FORWARD -o docker_bridge -j ACCEPT\"; echo $?'"
-  dosarray_execute_on "${HOST_NAME}" "" "sudo sh -c 'iptables -S | grep -E \"^-A FORWARD -o docker_bridge -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\" | echo $?'"
+
+
+#  echo -n "  Checking has network interface \"${DOSARRAY_HOST_INTERFACE_MAP[${IDX}]}\": "
+#  dosarray_execute_on "${HOST_NAME}" "ifconfig ${DOSARRAY_HOST_INTERFACE_MAP[${IDX}]}" "-q" 2>&1 > /dev/null
+#  RESULT="$?"
+#  if [ "$RESULT" == "0" ]
+#  then
+#    tput smso
+#    echo "OK"
+#    tput rmso
+#  else
+#    tput smso
+#    echo "FAIL ($RESULT)"
+#    tput rmso
+#    break
+#  fi
+#
+#  echo -n "  Checking that interface \"${DOSARRAY_HOST_INTERFACE_MAP[${IDX}]}\" has IP \"${DOSARRAY_PHYSICAL_HOSTS_PRIV[${IDX}]}\": "
+#  dosarray_execute_on "${HOST_NAME}" "ifconfig ${DOSARRAY_HOST_INTERFACE_MAP[${IDX}]} | grep \"inet addr\" | sed 's/^.*inet addr:\([^ ]*\).*$/\1/'" "-q" 2>&1 > /dev/null
+#  RESULT="$?"
+#  if [ ! "$RESULT" == "0" ]
+#  then
+#    tput smso
+#    echo "PRE-FAIL ($RESULT)"
+#    tput rmso
+#    break
+#  fi
+#
+#  if [ "${REMOTE_RESULT}" == "${DOSARRAY_PHYSICAL_HOSTS_PRIV[${IDX}]}" ]
+#  then
+#    tput smso
+#    echo "OK"
+#    tput rmso
+#  else
+#    tput smso
+#    echo "FAIL (${REMOTE_RESULT})"
+#    tput rmso
+#    break
+#  fi
+
+  echo -n "  Checking has network interface \"${DOCKER_BRIDGE}\": "
+  dosarray_execute_on "${HOST_NAME}" "ifconfig ${DOCKER_BRIDGE}" "-q" 2>&1 > /dev/null
+  RESULT="$?"
+  if [ "$RESULT" == "0" ]
+  then
+    tput smso
+    echo "OK"
+    tput rmso
+  else
+    tput smso
+    echo "FAIL ($RESULT)"
+    tput rmso
+    break
+  fi
+
+  echo -n "  Checking that interface \"${DOCKER_BRIDGE}\" has IP \"${DOSARRAY_PHYSICAL_HOSTS_PRIV[${IDX}]}\": "
+  dosarray_execute_on "${HOST_NAME}" "ifconfig ${DOCKER_BRIDGE} | grep \"inet addr\" | sed 's/^.*inet addr:\([^ ]*\).*$/\1/'" "-q" 2>&1 > /dev/null
+  RESULT="$?"
+  if [ ! "$RESULT" == "0" ]
+  then
+    tput smso
+    echo "PRE-FAIL ($RESULT)"
+    tput rmso
+    break
+  fi
+
+  if [ "${REMOTE_RESULT}" == "${DOSARRAY_PHYSICAL_HOSTS_PRIV[${IDX}]}" ]
+  then
+    tput smso
+    echo "OK"
+    tput rmso
+  else
+    tput smso
+    echo "FAIL (${REMOTE_RESULT})"
+    tput rmso
+    break
+  fi
+
+#  VIRTUAL_NETWORK="${DOSARRAY_VIRT_NETS[${IDX}]}0"
+#  dosarray_execute_on "${HOST_NAME}" "" "route | grep ${VIRTUAL_NETWORK}"
+#
+#  dosarray_execute_on "${HOST_NAME}" "" "sudo sh -c 'iptables -S | grep -E \"^-A FORWARD -o docker_bridge -j ACCEPT\"; echo $?'"
+#  dosarray_execute_on "${HOST_NAME}" "" "sudo sh -c 'iptables -S | grep -E \"^-A FORWARD -o docker_bridge -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\" | echo $?'"
 
   echo ""
 done
 
+tput sgr0
 echo "Done"
